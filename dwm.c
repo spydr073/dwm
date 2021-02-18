db3d5c42924b7dbe8122505804c5d53c530fe9f4
Killian Smith <spydr073@gmail.com>
2021-02-18 14:44:42 +0100

    fixed merge conflict

--- dwm.c
+++ dwm.c
@@ -2062,11 +2062,11 @@ updatebarpos(Monitor *m)
 	m->wy = m->my;
 	m->wh = m->mh;
 	if (m->showbar) {
-		m->wh = m->wh - vertpad - bh;
+		m->wh = m->wh - vertpad - m->bh;
 		m->by = m->topbar ? m->wy : m->wy + m->wh + vertpad;
-		m->wy = m->topbar ? m->wy + bh + vp : m->wy;
+		m->wy = m->topbar ? m->wy + m->bh + vp : m->wy;
 	} else
-		m->by = -bh - vp;
+		m->by = -m->bh - vp;
 }
 
 void
